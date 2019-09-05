d85ff28b0984825642981bca608666920052dbfe
Kishan Patel <kishan02patel@gmail.com>
2019-09-05 16:24:02 +1000

    Change folder structure

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Working');
+});
+
+app.listen(PORT, () => console.log('Server started at port: ', PORT));
